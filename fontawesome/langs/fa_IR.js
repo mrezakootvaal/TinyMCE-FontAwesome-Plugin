6666b01b0e49a0d5b3d2336d3b81d474431c6b3b
MohammadReza Kootvaal <info@famad.ir>
2015-05-03 12:30:32 +0430

    Create Translation to Persian (Farsi) Language
    Add Persian (فارسی) language file

---
+++ fontawesome/langs/fa_IR.js
@@ -0,0 +1,17 @@
+tinymce.addI18n('fa_IR', {
+    'Icons': '\u0646\u0645\u0627\u062F\u0647\u0627',
+    'Web Application': '\u0628\u0631\u0646\u0627\u0645\u0647\20\u06A9\u0627\u0631\u0628\u0631\u062F\u06CC\20\u0648\u0628',
+    'File Type': '%u0646%u0648%u0639%20%u067E%u0631%u0648%u0646%u062F%u0647',
+    'Spinner': '%u0627%u0633%u067E%u06CC%u0646%u0631',
+    'Form Control': '%u06A9%u0646%u062A%u0631%u0644%20%u0641%u0631%u0645',
+    'Currency': '%u0648%u0627%u062D%u062F%20%u067E%u0648%u0644',
+    'Text Editor': '%u0648%u06CC%u0631%u0627%u06CC%u0634%u06AF%u0631%20%u0645%u062A%u0646',
+    'Directional': 'Directional',
+    'Video Player': '%u067E%u062E%u0634%20%u06A9%u0646%u0646%u062F%u0647%20%u0648%u06CC%u062F%u0626%u0648',
+    'Brand': '%u0628%u0631%u0646%u062F',
+    'Medical': '%u067E%u0632%u0634%u06A9%u06CC',
+    'Transportation': '%u062D%u0645%u0644%20%u0648%20%u0646%u0642%u0644',
+    'Gender': '%u062C%u0646%u0633%u06CC%u062A',
+    'Payment': '%u067E%u0631%u062F%u0627%u062E%u062A',
+    'Chart': '%u0686%u0627%u0631%u062A'
+});
